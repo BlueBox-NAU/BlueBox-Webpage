35ba3e2ea198750b5041ec08201ac85c4aa4a849
zacharwl123 <zsw23@nau.edu>
2021-01-26 16:32:28 -0700

    first commit on new repo

---
+++ README.md
@@ -0,0 +1,2 @@
+# PeakLearn476
+Team Webpage: https://www.cefns.nau.edu/capstone/projects/CS/2021/Bluebox-F20/
---
+++ Webpage/assets/js/custom.js
@@ -0,0 +1,61 @@
+
+(function ($) {
+    "use strict";
+    var mainApp = {
+
+        main_fun: function () {
+
+            $('#carousel-slider').carousel({
+                interval: 3000
+            })
+
+
+            $(function () {
+                $('.scrollclass a').bind('click', function (event) {
+                    var $anchor = $(this);
+                    $('html, body').stop().animate({
+                        scrollTop: $($anchor.attr('href')).offset().top
+                    }, 1200, 'easeInOutExpo');
+                    event.preventDefault();
+                });
+            });
+
+
+            $(function () {
+                $.vegas('slideshow', {
+                    backgrounds: [
+                      { src: 'assets/img/1.jpg', fade: 1000, delay: 9000 },
+                      { src: 'assets/img/2.jpg', fade: 1000, delay: 9000 },
+
+
+                    ]
+                })('overlay', {
+
+                    src: 'assets/plugins/vegas/overlays/03.png'
+                });
+
+            });
+
+
+
+
+
+
+
+
+
+        },
+
+        initialization: function () {
+            mainApp.main_fun();
+
+        }
+
+    }
+    
+
+    $(document).ready(function () {
+        mainApp.main_fun();
+    });
+
+}(jQuery));
